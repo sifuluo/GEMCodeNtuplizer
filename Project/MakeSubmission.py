6a70ed9b007516acaf73df12130b8f8b3ce9746f
Sifu Luo <sifuluo@tamu.edu>
2021-11-23 23:25:55 +0100

    Updated MakeSubmission

--- Project/MakeSubmission.py
+++ Project/MakeSubmission.py
@@ -1,24 +1,25 @@
 import os, sys
 
-iteration = "Run4/"
-eositeration = "ME1/"
-cmssw_ver = "CMSSW_11_2_0"
+InPath = "Private/"
+OutPath = "MuGun/Ntuple/"
+cmssw_ver = "CMSSW_12_0_2_patch2"
 maxfile   = 5000
-IsRun4 = False
-if IsRun4:
-  datasets = ["RVSMPt10PU","RVSMPt10noPU","RVSMPt100PU","RVSMPt100noPU","RVSMPt1000PU","RVSMPt1000noPU","RVSMFlatPU","RVSMFlatnoPU"]
-  datasets = datasets + ["RVDMPt2PU","RVDMPt2noPU","RVDMPt10PU","RVDMPt10noPU","RVDMPt30PU","RVDMPt30noPU"]
-else:
-  datasets = ["DY","H9","H15","H30","H100"]
+#Run4
+# datasets = ["RVSMPt10PU","RVSMPt10noPU","RVSMPt100PU","RVSMPt100noPU","RVSMPt1000PU","RVSMPt1000noPU","RVSMFlatPU","RVSMFlatnoPU"]
+# datasets = datasets + ["RVDMPt2PU","RVDMPt2noPU","RVDMPt10PU","RVDMPt10noPU","RVDMPt30PU","RVDMPt30noPU"]
 
-datasets = ["RVSMPt1000noPU"]
+#Run3
+# datasets = ["DY","H9","H15","H30","H100"]
+
+#Private
+datasets = ["SMNoPU0","SMNoPU0a","SMNoPU1","SMNoPU1a","SMNoPU2","SMNoPU2a"]
 
 if len(sys.argv) == 1:
   process = 0
 else:
   process = int(sys.argv[1])
 
-eosdir = "/eos/user/s/siluo/Muon/" + eositeration
+eosdir = "/eos/user/s/siluo/" + OutPath
 curdir = os.path.abspath(os.path.curdir)
 subdir = os.path.join(curdir,"Submits")
 dsdir = "/afs/cern.ch/work/s/siluo/Muon/filenames/"
@@ -45,18 +46,18 @@ if process == 0 or process == 1:
     else: print( '  Existed: ' + ds + ' WIP directory' + mkeosinprogressdir)
 
     print('Dataset: ' + ds + ' finished creating directories')
-  print('Iteration: ' + eositeration + ' finished creating directories')
+  print('Iteration: ' + OutPath + ' finished creating directories')
 
 # Make Submission scripts
 if process == 0 or process == 2:
   for ds in datasets:
-    nf = len(open(dsdir+iteration+ds+".txt").readlines())
+    nf = len(open(dsdir+InPath+ds+".txt").readlines())
     lines = []
     lines.append("Proxy_path   = /afs/cern.ch/user/s/siluo/x509up\n")
-    lines.append("arguments    = $(Proxy_path) $(ProcID) "+iteration+ds+" 0 "+eositeration+ds+"\n")
+    lines.append("arguments    = $(Proxy_path) $(ProcID) "+InPath+ds+" 0 "+OutPath+ds+"\n")
     lines.append("executable   = MultiSub.sh\n")
     lines.append("max_retries  = 10\n")
-    lines.append("+JobBatchName= " + iteration+ds +"\n")
+    lines.append("+JobBatchName= " + InPath+ds +"\n")
     lines.append("output       = logs"+ds+"/log_$(ClusterID)_$(ProcID).out\n")
     lines.append("error        = logs"+ds+"/log_$(ClusterID)_$(ProcID).err\n")
     lines.append("log          = logs"+ds+"/log_$(ClusterID)_$(ProcID).log\n")
@@ -85,7 +86,7 @@ if process == 0 or process == 3:
   lines.append("j=${4:-0}\n")
   lines.append("i=$(($2+5000*$j))\n")
   lines.append("\n")
-  lines.append("existname=/eos/user/s/siluo/Muon/${3}/out_${i}.root\n")
+  lines.append("existname=/eos/user/s/siluo/${5}/out_${i}.root\n")
   lines.append("if [ -e $existname ]\n")
   lines.append("then\n")
   lines.append("  echo 'File already processed'\n")
@@ -99,7 +100,7 @@ if process == 0 or process == 3:
   lines.append("cd "+curdir+"/\n")
   lines.append("cmsRun NtuplizeSIM.py ifile=$i dataset=$3 outputtag=$5\n")
   lines.append("\n")
-  lines.append("filename=/eos/user/s/siluo/Muon/${3}/InProgress/out_${i}.root\n")
+  lines.append("filename=/eos/user/s/siluo/${5}/InProgress/out_${i}.root\n")
   lines.append("filesize=$(stat -c %s $filename)\n")
   lines.append("if [ $filesize -gt 1000 ]\n")
   lines.append("then\n")
@@ -135,3 +136,13 @@ if process == 0 or process == 4:
   mode = "755"
   os.chmod(fn,int(mode,base=8));
   print('Submitting shell script created: '+ fn)
+
+if process == 0 or process == 5:
+  lines = []
+  lines.append("cmsRun NtuplizeSIM.py ifile=0 dataset={} outputtag={}\n".format(InPath+datasets[0],OutPath+datasets[0]))
+  fn = "TestRun.sh"
+  f = open(fn,"w")
+  f.writelines(lines)
+  mode = "755"
+  os.chmod(fn,int(mode,base=8));
+  print('TestRun shell script created: '+ fn)
